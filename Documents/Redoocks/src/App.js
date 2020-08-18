dc9c8e1c5bdbad19244d5ddc8c3ff43238839c99
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-18 17:24:22 +0900

    Deleting To Dos

--- Documents/Redoocks/src/App.js
+++ Documents/Redoocks/src/App.js
@@ -2,17 +2,25 @@ import React, {useReducer, useState} from 'react';
 import UserContextProvider, { useSetLang } from './context';
 import Screen from './Screen';
 import translation from "./translation";
+import { v4 as uuid } from "uuid";
 
 const initialState = {
   toDos: []
 };
 
 const ADD = "add";
+const DEL = "del";
 
 const reducer = (state, action) => {
   switch(action.type) {
     case ADD:
-      return {toDos: [...state.toDos, {text: action.payload}]}
+      return {
+        toDos: [...state.toDos, {text: action.payload, id: uuid()}]
+      };
+    case DEL:
+      return {
+        toDos: state.toDos.filter(todo => todo.id !== action.payload)
+      }
     default:
       return;
   }
@@ -25,6 +33,7 @@ function App() {
   const onSumbit = e => {
     e.preventDefault();
     dispatch({type: ADD, payload: newToDo});
+    setNewToDo("");
   };
   const onChange = e => {
     const {
@@ -46,7 +55,13 @@ function App() {
         onChange={onChange}
       />
     </form>
-    <h4>{state.toDos.map(todo => <li>{todo.text}</li>)}</h4>
+    <h4>{state.toDos.map(todo => (
+      <li>
+      <span>{todo.text}</span>
+      <button onClick={() => dispatch({type: DEL, payload: todo.id})} >❌</button>
+      </li>
+      ))}
+    </h4>
     </>
   )
 }
