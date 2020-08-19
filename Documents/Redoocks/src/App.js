7cc625c42e23b3c7cf0dd3435d46ff350b978c1d
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-19 13:19:48 +0900

    Completing To Dos, UnCompleting To Dos

--- Documents/Redoocks/src/App.js
+++ Documents/Redoocks/src/App.js
@@ -5,22 +5,41 @@ import translation from "./translation";
 import { v4 as uuid } from "uuid";
 
 const initialState = {
-  toDos: []
+  toDos: [],
+  completed: []
 };
 
 const ADD = "add";
 const DEL = "del";
+const COMPLETE = "complete";
+const UNCOMPLETE = "uncomplete";
 
 const reducer = (state, action) => {
   switch(action.type) {
     case ADD:
       return {
+        ...state,
         toDos: [...state.toDos, {text: action.payload, id: uuid()}]
       };
     case DEL:
       return {
+        ...state,
         toDos: state.toDos.filter(todo => todo.id !== action.payload)
-      }
+      };
+    case COMPLETE:
+      const target = state.toDos.find(todo => todo.id === action.payload);
+      return {
+        ...state,
+        toDos: state.toDos.filter(todo => todo.id !== action.payload),
+        completed: [...state.completed, {...target}]
+      };
+      case UNCOMPLETE:
+        const aTarget = state.completed.find(todo => todo.id === action.payload);
+        return {
+          ...state,
+          toDos: [...state.toDos, {...aTarget}],
+          completed: state.completed.filter(todo => todo.id !== action.payload)
+        }
     default:
       return;
   }
@@ -55,13 +74,35 @@ function App() {
         onChange={onChange}
       />
     </form>
-    <h4>{state.toDos.map(todo => (
-      <li>
+    <ul>
+      {state.toDos.length !== 0 &&
+      <>
+        <h2>Todo </h2>
+        <h4>{state.toDos.map(todo => (
+          <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={() => dispatch({type: DEL, payload: todo.id})} >❌</button>
+          <button onClick={()=> dispatch({type: COMPLETE, payload: todo.id})}>✅</button>
+          </li>
+        ))}
+          
+        </h4>
+      </>}
+    </ul>
+    <ul>
+    {state.completed.length !==0  &&
+    <>
+    <h2>Completed</h2>
+    <h4>{state.completed.map(todo =>
+    <li key={todo.id}>
       <span>{todo.text}</span>
       <button onClick={() => dispatch({type: DEL, payload: todo.id})} >❌</button>
-      </li>
-      ))}
+      <button onClick={() => dispatch({type: UNCOMPLETE, payload: todo.id})}>🙅🏼‍♂️</button>
+    </li>
+    )}
     </h4>
+    </>}
+    </ul>
     </>
   )
 }
