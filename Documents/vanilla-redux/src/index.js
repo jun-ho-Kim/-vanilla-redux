9778d45ad384618b34295625f07a5ad7d96c7925
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-24 17:40:15 +0900

    Subscribe

--- Documents/vanilla-redux/src/index.js
+++ Documents/vanilla-redux/src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerHTML = 0;
+
 const countModifier = (count = 0, action) => {
   console.log("count:", count, "action:", action);
   if(action.type === "ADD") {
@@ -17,10 +19,20 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD"});
-countStore.dispatch({ type: "ADD"});
-countStore.dispatch({ type: "ADD"});
-countStore.dispatch({ type: "ADD"});
-countStore.dispatch({ type: "MINUS"});
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type:"ADD"});
+};
+
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"});
+}
 
 console.log("countStore.getState(): ",countStore.getState());
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
