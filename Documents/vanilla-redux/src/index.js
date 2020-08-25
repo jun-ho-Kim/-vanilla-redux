9d46262ace50f92af5e9c279a9e912da3a27ce8d
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-25 10:47:14 +0900

    count Refactor

--- Documents/vanilla-redux/src/index.js
+++ Documents/vanilla-redux/src/index.js
@@ -6,14 +6,18 @@ const number = document.querySelector("span");
 
 number.innerHTML = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   console.log("count:", count, "action:", action);
-  if(action.type === "ADD") {
-    return count += 1;
-  } else if (action.type === "MINUS") {
-    return count -=1;
-  } else {
-    return count;
+  switch(action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 }
 
