c9d5ccaaf69887f52a7ef9e79b2f3db4a6f36a44
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-25 14:28:48 +0900

    State Mutation

--- Documents/vanilla-redux/src/index.js
+++ Documents/vanilla-redux/src/index.js
@@ -4,37 +4,32 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-// const ADD_TODO = "ADD_TODO";
-// const DEL_TODO = "DEL_TODO";
+const ADD_TODO = "ADD_TODO";
+const DEL_TODO = "DEL_TODO";
 
-// const reducer = (count = 0, action) => {
-//   console.log("count:", count, "action:", action);
-//   switch(action.type) {
-//     case ADD_TODO:
-//       return [{text: action.text}];
-//     case DEL_TODO:
-//       return [];
-//     default:
-//       return [];
-//   }
-// }
-
-// const todoStore = createStore(reducer);
-
-const createToDo = toDo => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const reducer = (state = [], action) => {
+  console.log("state:", state, "action:", action);
+  switch(action.type) {
+    case ADD_TODO:
+      return [...state, {text: action.text}];
+    case DEL_TODO:
+      return [];
+    default:
+      return [];
+  }
 }
 
+const todoStore = createStore(reducer);
+
+todoStore.subscribe(() => console.log("todoStore.getState(): ",todoStore.getState()));
 const onSumbit = (e) => {
   e.preventDefault();
   const todo = input.value;
-  createToDo(todo);
+  // createToDo(todo);
   input.value = "";
-  // todoStore.dispatch({type:ADD_TODO, text: todo});
+  todoStore.dispatch({type:ADD_TODO, text: todo});
 }
 
-// console.log("todoStore.getState(): ",todoStore.getState());
-
 form.addEventListener("submit", onSumbit);
+
+console.log("todoStore.getState(): ",todoStore.getState());
