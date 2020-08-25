64113f5a4d6507c8c95e68191960e0f1aac8fb57
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-25 16:43:57 +0900

    Delete To Do

--- Documents/vanilla-redux/src/index.js
+++ Documents/vanilla-redux/src/index.js
@@ -7,29 +7,65 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DEL_TODO = "DEL_TODO";
 
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DEL_TODO,
+    id
+  };
+};
+
 const reducer = (state = [], action) => {
   console.log("state:", state, "action:", action);
   switch(action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text}];
+      return [...state, {text: action.text, id: Date.now()}];
     case DEL_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== parseInt(action.id));
     default:
-      return [];
+      return state;
   }
-}
+};
+
 
 const todoStore = createStore(reducer);
 
-todoStore.subscribe(() => console.log("todoStore.getState(): ",todoStore.getState()));
+todoStore.subscribe(() => console.log(todoStore.getState()));
+
+const dispatchDeleteToDo = e => {
+  e.preventDefault();
+  const id = e.target.parentNode.id;
+  todoStore.dispatch(deleteToDo(id));
+}
+const paintToDos = () => {
+  const toDos = todoStore.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("btn");
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    delBtn.innerHTML = "❌";
+    li.appendChild(delBtn);
+    ul.appendChild(li);
+    delBtn.addEventListener("click", dispatchDeleteToDo);
+  })
+}
+
+
+todoStore.subscribe(paintToDos);
+
 const onSumbit = (e) => {
   e.preventDefault();
   const todo = input.value;
-  // createToDo(todo);
   input.value = "";
-  todoStore.dispatch({type:ADD_TODO, text: todo});
+  todoStore.dispatch(addToDo(todo));
 }
 
 form.addEventListener("submit", onSumbit);
-
-console.log("todoStore.getState(): ",todoStore.getState());
