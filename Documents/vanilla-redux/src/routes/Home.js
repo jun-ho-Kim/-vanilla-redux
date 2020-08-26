1fb3c85e295fab4a6fda47104b9eed0e40725962
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-08-26 16:41:54 +0900

    React-redux mapStateToProps

--- Documents/vanilla-redux/src/routes/Home.js
+++ Documents/vanilla-redux/src/routes/Home.js
@@ -1,1 +1,33 @@
-export default () => "Home";
+import React, {useState} from "react";
+import {connect} from "react-redux";
+
+const Home = ({toDos}) => {
+    const [text, setText] = useState("");
+    function onChange(e) {
+        setText(e.target.value);
+    };
+    function onSubmit(e) {
+        e.preventDefault();
+        setText("");
+    };
+    return (
+        <>
+        <form onSubmit={onSubmit}>
+            <input 
+                value={text} 
+                onChange={onChange} 
+                placeholder="Write the todo"
+            />
+            <button>ADD</button>
+        </form>
+        <ul>{JSON.stringify(toDos)}</ul>
+        </>
+    )
+};
+
+function mapStateToProp(state) {
+    return {
+        toDos: state
+    }
+};
+export default connect(mapStateToProp)(Home);
